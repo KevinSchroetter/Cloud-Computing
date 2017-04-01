247ab61c56dc530f6cc9a2e68e7321da9265fbb6
KevinSchroetter <kevin.schroetter@student.reutlingen-university.de>
2017-04-01 22:31:39 +0200

    included the competed tutorial from myself with some additions accordint to some tests i made with CLIENTSIDE javascript execution using express.static

---
+++ Project/functions/javaScriptTestFunctionServerSide.js
@@ -0,0 +1,5 @@
+//called on the node server side and NOT on the client side
+
+exports.testAlert = function(){
+	alert("okay test");
+}
---
+++ Project/public/javaScript/javaScriptTestFunctionClientSide.js
@@ -0,0 +1,5 @@
+//this function will be called in index.js
+//so you can see, that javascript does not need any includes within javascript
+function testAlert(){
+	alert('ok');
+}
